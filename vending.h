e9151df384657107b9d93f5832ebe605ad14993d
mingseobi <22200078@handong.ac.kr>
2023-05-20 12:45:31 +0900

    함수 파라미터 수정 완료

--- vending.h
+++ vending.h
@@ -4,8 +4,14 @@ typedef struct
     char item_Name[100]; //물품의 이름
     int item_Count; //물품의 수량
     int price;  // 개당 가격 
-    char is_pointsave; //포인트 적립 유무
+    char is_pointsave[2]; //포인트 적립 유무
 } Item;
+
+typedef struct
+{
+    int input_cash;
+    int reward_point;
+} Cash;
 
 int menu(); // 메뉴
 
@@ -21,14 +27,12 @@ void save_file(Item *item, int count, char filename[100]); // 파일 저장 함�
 
 int load_file(Item *item, char filename[100]); // 파일에서 읽어오는 함수
 
-void input_money(Item *item, int count); // 현금을 자판기에 투입하는 함수
+void input_money(Cash cash); // 현금을 자판기에 투입하는 함수
 
-void buy_item(Item *item, int count); // 물건을 구매하는 함수
+void buy_item(Item *item, Cash cash, int count); // 물건을 구매하는 함수
 
-void math_reward(Item *item, int count); // 포인트 계산 함수
+void math_reward(Cash cash ,int buy_money); // 포인트 계산 함수
 
-void check_money(Item *item, int count); // 투입된 금액을 확인하는 함수
+void check_money(Cash cash); // 투입된 금액을 확인하는 함수
 
-void check_point(Item *item, int count); // 현재 보유한 포인트 잔액 확인 기능
-
-void exchange_money(Item *item, int count); // 거스름돈 함수
+void check_point(Cash cash); // 현재 보유한 포인트 잔액 확인 기능
